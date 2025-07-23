e5965cb5c129cd59c1dfb417c2e517f12fb3c06d
Shayesta <nausheenshayesta21@gmail.com>
2025-07-23 19:29:35 -0400

    Selenium Grid integration

--- src/test/java/testbase/BaseClass.java
+++ src/test/java/testbase/BaseClass.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -13,11 +14,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -40,11 +44,42 @@ public class BaseClass {
 		
 		logger= LogManager.getLogger(this.getClass());//log4j entry do it only once in here
 		
+		//***********for execution in Grid set up
+		
+		//for operating system
+		if(p.getProperty("Execution_environment").equalsIgnoreCase("remote")){
+			
+			DesiredCapabilities dc= new DesiredCapabilities();
+			if(os.equalsIgnoreCase("windows")) {
+				dc.setPlatform(Platform.WIN11);
+			}else if(os.equalsIgnoreCase("mac")) {
+				dc.setPlatform(Platform.MAC);
+			}else {
+				System.out.println("no matching os");
+				
+			}
+	
+		// for browser
+			switch(br.toLowerCase()) {
+			case "chrome":  dc.setBrowserName("chrome"); break;
+			case "firefox": dc.setBrowserName("Firefox"); break;
+			case "edge":    dc.setBrowserName("edge");break;
+			default: System.out.println("Nomatching browser");return;
+			
+			}
+		
+		driver= new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),dc);
+			
+		}
+		
+		//for execution in the local environment
+		if(p.getProperty("Execution_environment").equalsIgnoreCase("local")) {
 		switch(br.toLowerCase()) {
 		case "chrome":driver=new ChromeDriver(); break;
 		case "edge": driver= new EdgeDriver(); break;
 		case "firefox" : driver =new FirefoxDriver(); break;
 		default:System.out.println("This is an invalid browser"); return;
+		}
 		}
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
